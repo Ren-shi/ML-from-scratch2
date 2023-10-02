7a769ed1a173182347d0b516e4a7f4118b1f8e93
Ren-shi <brandonlesch112@gmail.com>
2023-10-02 08:55:57 +0200

    Customized chart in features to be interactive

--- web/js/display.js
+++ web/js/display.js
@@ -13,6 +13,8 @@ function createRow(container,studentName,samples){
 
         const sampleContainer=document.createElement("div");
         sampleContainer.id="sample_"+id;
+        sampleContainer.onclick=()=>
+            handleClick(sample,false);
         sampleContainer.classList.add("sampleContainer");
 
         const sampleLabel=document.createElement("div");
@@ -26,3 +28,29 @@ function createRow(container,studentName,samples){
         row.appendChild(sampleContainer);
     }
 }
+
+function handleClick(sample,doscroll=true){
+    if(sample==null){
+        [...document.querySelectorAll('.emphasize')].
+            forEach((e)=>e.classList.remove('emphasize'));
+        return;
+    }
+    const el=document.getElementById(
+        "sample_"+sample.id
+        );
+    if(el.classList.contains('emphasize')){
+        el.classList.remove("emphasize");
+        chart.selectSample(null);
+        return;
+    }
+    [...document.querySelectorAll('.emphasize')].
+        forEach((e)=>e.classList.remove('emphasize'));
+    el.classList.add("emphasize");
+    if(doscroll){
+        el.scrollIntoView({
+            behavior:'auto',
+            block:'center'
+        });
+    }
+    chart.selectSample(sample);
+ }
